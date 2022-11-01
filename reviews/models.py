89e2a5cd5ae56193fb101d43287e158ac423ceb1
Aleksandra Oksenchuk <aleksandra.oksenchuk@gmail.com>
2022-11-01 23:19:12 +0300

    Create model for reviews

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from places.models import Hotel
 
-# Create your models here.
+
+class Review(models.Model):
+    name = models.CharField(max_length=30)
+    date = models.DateField(auto_now_add=True)
+    review = models.TextField()
+    rating = models.PositiveSmallIntegerField()
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["date"]
